b76aaac54ce7a085a84b9a7a77e1102d05c86ec1
Slyther <joseph.a.griff_07@hotmail.com>
2015-06-22 21:55:33 -0600

    some further additions, nothing major (or useful)

--- ProCulturaBackEnd/Controllers/LoginController.cs
+++ ProCulturaBackEnd/Controllers/LoginController.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Domain.Entities;
 using ProCulturaBackEnd.Models;
+using Domain.Services;
+
 
 namespace ProCulturaBackEnd.Controllers
 {
     public class LoginController : ApiController
     {
+        readonly IReadOnlyRepository _readOnlyRepository;
+        readonly IWriteOnlyRepository _writeOnlyRepository;
+
+        public LoginController(IReadOnlyRepository readOnlyRepository, IWriteOnlyRepository writeOnlyRepository)
+        {
+            _readOnlyRepository = readOnlyRepository;
+            _writeOnlyRepository = writeOnlyRepository;
+        }
         // GET api/values/5
         public UserModel Get(int id)
         {
             return new UserModel()
             {
                 id = 1,
-                UserName = "hello",
+                Email = "lkajdalkj",
+                Name = "hello",
                 Password = "dskjshfjdh"
             };
         }
@@ -24,6 +36,11 @@ namespace ProCulturaBackEnd.Controllers
         // POST api/values
         public UserModel Post([FromBody]UserModel model)
         {
+            var user = _readOnlyRepository.FirstOrDefault<User>(x => x.Email == model.Email);
+            if (user.CheckPassword(model.Password))
+            {
+                
+            }
             return model;
         }
     }
--- ProCulturaBackEnd/Models/UserModel.cs
+++ ProCulturaBackEnd/Models/UserModel.cs
@@ -12,7 +12,8 @@ namespace ProCulturaBackEnd.Models
     {
         [Key]
         public int id { get; set; }
-        public string UserName { get; set; }
         public string Password { get; set; }
+        public  string Name { get; set; }
+        public  string Email { get; set; }
     }
 }
