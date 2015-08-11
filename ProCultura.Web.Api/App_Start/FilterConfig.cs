aa81cf0a7c3ff2be3dbd68973b9720826086408a
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-08-10 23:33:12 -0600

    Added Exception filter class

--- ProCultura.Web.Api/App_Start/FilterConfig.cs
+++ ProCultura.Web.Api/App_Start/FilterConfig.cs
@@ -2,11 +2,24 @@
 
 namespace ProCultura.Web.Api
 {
+    using Autofac;
+
+    using ProCultura.CrossCutting.L10N;
+    using ProCultura.Web.Api.Filters;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            var builder = new ContainerBuilder();
+            var container = builder.Build();
+
+            using(var scope = container.BeginLifetimeScope())
+            {
+                var service = scope.Resolve<ILocalizationService>();
+                filters.Add(new ProCulturaExceptionFilterAttribute(service));
+            }
         }
     }
 }
