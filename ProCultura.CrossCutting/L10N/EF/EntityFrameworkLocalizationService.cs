101115f63253c94d2d6066aac8ba7dcdf6a4f28e
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-08-14 23:28:23 -0600

    Preparing for changes

---
+++ ProCultura.CrossCutting/L10N/EF/EntityFrameworkLocalizationService.cs
@@ -0,0 +1,20 @@
+﻿namespace ProCultura.CrossCutting.L10N.EF
+{
+    using System.Linq;
+
+    public class EntityFrameworkLocalizationService : ILocalizationService
+    {
+
+        private readonly LocalizationContext _context;
+        public EntityFrameworkLocalizationService(LocalizationContext context)
+        {
+            this._context = context;
+        }
+
+        public string GetLocalizedString(string resourceKey, string languageId)
+        {
+            //var result = _context.LocalizedEntries.FirstOrDefault()
+            return string.Empty;
+        }
+    }
+}
---
+++ ProCultura.CrossCutting/L10N/EF/LocalizedEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace ProCultura.CrossCutting.L10N.EF
+{
+    public class LocalizedEntry
+    {
+        public string LanguageId { get; set; }
+
+        public string EntryKey { get; set; }
+
+        public string Value { get; set; }
+    }
+}
