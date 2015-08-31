5e51877e1f3b4c78cca09f32c173336a14ea7a12
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-08-30 20:39:43 -0600

    Added EventController
    Added seed event

---
+++ ProCultura.Web.Api/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+
+namespace ProCultura.Web.Api.Controllers
+{
+    using Procultura.Application.DTO.Events;
+    using Procultura.Application.Services.Events;
+
+    public class EventController : ApiController
+    {
+        private readonly IEventsAppService _eventsAppService;
+
+        public EventController(IEventsAppService eventsAppService)
+        {
+            this._eventsAppService = eventsAppService;
+        }
+
+        public EventModel Get(int eventId)
+        {
+            return _eventsAppService.GetEventWithId(eventId);
+        }
+
+        public EventModel Post(NewEventModel request)
+        {
+            return _eventsAppService.CreateEvent(request);
+        }
+
+        public EventModel Put(NewEventModel request)
+        {
+            return _eventsAppService.UpdateEvent(request);
+        }
+
+        public EventModel Delete(int eventId)
+        {
+            return _eventsAppService.DeleteEvent(eventId);
+        }
+    }
+}
