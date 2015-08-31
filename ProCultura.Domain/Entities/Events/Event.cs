a382d1b909838cf09e66584cee24811bc88af9f0
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-08-30 18:38:17 -0600

    Created event entity

---
+++ ProCultura.Domain/Entities/Events/Event.cs
@@ -0,0 +1,35 @@
+﻿namespace ProCultura.Domain.Entities.Events
+{
+    using System;
+
+    /// <summary>
+    /// En object representing the Event entity
+    /// </summary>
+    public class Event : IEntity
+    {
+        /// <summary>
+        /// The unique Id for the Event
+        /// </summary>
+        public int EventId { get; set; }
+
+        /// <summary>
+        /// The event name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A summary of what this event is about
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// The Date on which this event will ocurr
+        /// </summary>
+        public DateTime BeginDate { get; set; }
+
+        /// <summary>
+        /// The Location where the event will be hold
+        /// </summary>
+        public string Location { get; set; }
+    }
+}
