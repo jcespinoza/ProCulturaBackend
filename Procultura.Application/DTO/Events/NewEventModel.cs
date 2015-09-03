26b285c4b78507669642a26c941e33813b6bd418
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-09-02 20:45:24 -0600

    Fiexd bug when creating new event

--- Procultura.Application/DTO/Events/NewEventModel.cs
+++ Procultura.Application/DTO/Events/NewEventModel.cs
@@ -6,11 +6,11 @@
     {
         public int EventId { get; set; }
         public string Name { get; set; }
-        public string Summary { get; set; }
+        public string ShortDescription { get; set; }
         public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Location { get; set; }
-        public string Description { get; set; }
+        public string LongDescription { get; set; }
         public string ImageUrl { get; set; }
         public string Status { get; set; }
     }
