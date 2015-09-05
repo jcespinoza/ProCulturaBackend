3114578e01675605d9f1b006aa06bd78a5523179
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-09-05 14:08:24 -0600

    Deleted the Exception property from ResponseBase
    Also deleted the MarkedWithError extension mrthod since it won't be used anymore

--- Procultura.Application/DTO/ResponseBase.cs
+++ Procultura.Application/DTO/ResponseBase.cs
@@ -1,10 +1,7 @@
 ﻿namespace Procultura.Application.DTO
 {
-    using System;
-
     public class ResponseBase
     {
-        public Exception Exception { get; set; }
         public string Message { get; set; }
     }
 }
