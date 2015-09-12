adb11ffa7bdb43214dbd3a0b6a00367377b66850
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-09-11 23:51:21 -0600

    Created Localization Filter

--- ProCultura.Web.Api/Filters/LocalizationFilterAttribute.cs
+++ ProCultura.Web.Api/Filters/LocalizationFilterAttribute.cs
@@ -1,23 +1,54 @@
 ﻿namespace ProCultura.Web.Api.Filters
 {
+    using System.Linq;
+    using System.Net.Http;
     using System.Web.Http.Filters;
 
+    using Procultura.Application.DTO;
     using ProCultura.CrossCutting.L10N;
+    using ProCultura.CrossCutting.Settings;
 
     public class LocalizationFilterAttribute : ActionFilterAttribute
     {
         private readonly ILocalizationService _localizationService;
-
         public LocalizationFilterAttribute(ILocalizationService localizationService)
         {
-            _localizationService = localizationService;
+            this._localizationService = localizationService;
         }
 
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnActionExecuted(actionExecutedContext);
-            var response = actionExecutedContext.Response.Content;
-            //TODO: Parse the string
+            var requestLanguage = GetLanguageFromRequest(actionExecutedContext.Request);
+            ReplaceMessageKeys(actionExecutedContext.Response.Content, requestLanguage);
+        }
+
+        private string GetLanguageFromRequest(HttpRequestMessage request)
+        {
+            var headers = request.Headers;
+            var acceptLanguageHeader = headers.AcceptLanguage;
+            var firstLanguage = acceptLanguageHeader.FirstOrDefault();
+            if (firstLanguage != null)
+            {
+                return firstLanguage.ToString().Substring(0, 2);
+            }
+            return AppStrings.EnglishCode;
+        }
+
+        private void ReplaceMessageKeys(HttpContent httpContent, string languageId)
+        {
+            var content = httpContent as ObjectContent;
+            if (content != null)
+            {
+                var responseValue = content.Value as ResponseBase;
+                if (responseValue != null)
+                {
+                    var messageKey = responseValue.Message;
+                    var replacement = _localizationService.GetLocalizedString(messageKey, languageId);
+                    responseValue.Message = replacement;
+                }
+            }
+            
         }
     }
 }
