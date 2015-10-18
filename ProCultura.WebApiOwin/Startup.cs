fc4ded62a70b2653153014768ce2798a6bcc9975
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2015-10-17 21:20:13 -0600

    Added Autofac configuration

--- ProCultura.WebApiOwin/Startup.cs
+++ ProCultura.WebApiOwin/Startup.cs
@@ -8,11 +8,37 @@ using Owin;
 
 namespace ProCultura.WebApiOwin
 {
+    using System.Reflection;
+    using System.Web.Http;
+
+    using Autofac;
+    using Autofac.Integration.WebApi;
+
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            var builder = new ContainerBuilder();
+
             ConfigureAuth(app);
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Get your HttpConfiguration. In OWIN, you'll create one
+            // rather than using GlobalConfiguration.
+            var config = new HttpConfiguration();
+
+            // Run other optional steps, like registering filters,
+            // per-controller-type services, etc., then set the dependency resolver
+            // to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // Register the Autofac middleware FIRST.
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
         }
     }
 }
